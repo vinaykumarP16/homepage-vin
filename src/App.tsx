0a57304e57c90e1125568fa433ed24c2e95d79ae
vinaykumarp16 <vinaykumar.p1995v@gmail.com>
2023-07-07 20:49:42 +0530

    feat(sites): adding sites + messages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { Homepage } from "./Homepage/Homepage";
 import { Deadlines } from "./Deadlines/Deadlines";
-import {  IconButton } from "@mui/material";
+import { IconButton } from "@mui/material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import MilestonesTracker from "./MilestonesTracker/MilestonesTracker";
 
@@ -11,18 +11,19 @@ function App() {
   return (
     <div className="App">
       <Homepage />
-      <h1 style={{ color: "red" }}>You Can</h1>
+      <h1 style={{ color: "red" }}>You Can | Never Stop Dreaming </h1>
 
       <IconButton onClick={() => setShowDeadlines(!showDeadlines)}>
-        <VisibilityIcon color="warning" />
+        <VisibilityIcon color="warning" /><span style={{ color: "white", marginLeft:'10px', fontSize:'25px' }}>16</span>
       </IconButton>
       {showDeadlines && (
         <div>
           <h4>Achievements / Goals to be done by September</h4> <Deadlines />
-          <MilestonesTracker/>
+          <MilestonesTracker />
         </div>
       )}
-      <h1 style={{ color: "red" }}>Believe</h1>
+      <h1 style={{ color: "red" }}> Think Big, Do Big | Believe </h1>
+      
     </div>
   );
 }

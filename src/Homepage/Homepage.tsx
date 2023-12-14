10994cf1a82f267d760d064bdd2e98e408ae4f55
VINAY KUMAR P <vinaykumar.p1995v@gmail.com>
2023-12-14 05:23:02 +0000

    updating login in vite url change

--- src/Homepage/Homepage.tsx
+++ src/Homepage/Homepage.tsx
@@ -177,7 +177,7 @@ export const Homepage = () => {
     },
     {
       name: "jarvis server Vite",
-      link: "http://127.0.0.1:3000/app",
+      link: "http://127.0.0.1:3000/app/login",
       color: "Blue",
     },
     {
